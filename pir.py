9dc6217dc5eee1e64e2b7cf59568a9a0d3ba9704
Os <>
2023-08-10 13:07:57 +0000

    additional pir sensor

--- pir.py
+++ pir.py
@@ -2,17 +2,21 @@ from gpiozero import MotionSensor, Device
 from signal import pause
 import time
 
-pir = MotionSensor(23)
+pir_1 = MotionSensor(23, sample_rate=10, queue_len=40,threshold=0.2)
+pir_2 = MotionSensor(24, sample_rate=10, queue_len=40,threshold=0.2)
 
 # pir.when_motion = lambda: print("Motion detected!")
 # pir.when_no_motion = lambda: print("All is quiet...")
 
 while True:
-    if pir.motion_detected:
-        print("Motion detected!")
+    if pir_1.motion_detected:
+        print("pir_1 detected!")
+
+    elif pir_2.motion_detected:
+        print("pir_2 detected!")
     else:
-        print("All is quiet...")
-        time.sleep(1)
+        print("_________________")
+        # time.sleep(1)
 
 pause()
 
